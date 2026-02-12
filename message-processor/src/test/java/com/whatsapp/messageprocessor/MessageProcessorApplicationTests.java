5b5f1698147d03724f23de299a9ca30e7fc8b9c7
Thanh14013 <thanh14704@gmail.com>
2026-02-12 13:45:34 +0700

    first commit

---
+++ message-processor/src/test/java/com/whatsapp/messageprocessor/MessageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatsapp.messageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ scheduled-jobs/src/main/java/com/whatsapp/scheduledjobs/ScheduledJobsApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp.scheduledjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledJobsApplication.class, args);
+    }
+
+}
